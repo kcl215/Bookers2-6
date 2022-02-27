cc4b8a3a21eae8e9ec6d8c704857397f622853ad
EC2 Default User <ec2-user@ip-172-31-25-149.ec2.internal>
2022-02-27 06:35:10 +0000

    [ADD]いいね機能の非同期通信化

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require rails-ujs
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("../card")
+require("jquery")
